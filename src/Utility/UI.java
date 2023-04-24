d513d655f4719b78e33319ba9d253cb3b369dfa7
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2023-04-24 08:54:57 +0200

    created IO and UI classes

---
+++ src/Utility/UI.java
@@ -0,0 +1,21 @@
+package Utility;
+
+import java.util.Scanner;
+
+public class UI {
+    Scanner scan;
+
+
+    public UI (){
+        scan = new Scanner(System.in);
+    }
+    public void displayMessage(String msg){
+
+        System.out.println(msg);
+    }
+    public String getInput(String msg){
+        displayMessage(msg);
+        String input = scan.nextLine();
+        return input;
+    }
+}
