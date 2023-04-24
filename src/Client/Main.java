f6e40f506dc7e00fa496c4137c1970a2100028c2
lassekh <lasse.kh.92@gmail.com>
2023-04-24 11:09:29 +0200

    Application launch method
    Main updated

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -1,4 +1,10 @@
 package Client;
 
+import Entity.Application;
+
 public class Main {
+    public static void main(String[] args) {
+        Application streaming = new Application();
+        streaming.launchApplication();
+    }
 }
--- src/Entity/Application.java
+++ src/Entity/Application.java
@@ -11,13 +11,21 @@ import java.util.Set;
 public class Application {
     private List<User> users;
     private Set<Media> medias;
-    private IO io;
-    private UI ui;
+    private IO io = new IO();
+    private UI ui = new UI();
     public void launchApplication(){
-
+        startMenu();
     }
     public void startMenu(){
-
+        String input = ui.getInput("Do you want to 1. Create user or 2. Login");
+        if(input.equals("1")){
+            createUser();
+        } else if(input.equals(2)){
+            login();
+        } else{
+            ui.displayMessage("Try again");
+            startMenu();
+        }
     }
     public void createUser(){
 
@@ -28,10 +36,10 @@ public class Application {
     public void mainMenu(){
 
     }
-    public Media search(String input){
+    public void search(String input){
         //todo: add return statement
     }
-    public Set<Media> filter(int i){
+    public void filter(int i){
         //todo: add return statement
     }
     public void chooseMedia(){
