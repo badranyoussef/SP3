ac2f733cd082810e0b5825f828aa64314bafbdea
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-04-24 09:47:39 +0200

    Updated

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -1,5 +1,6 @@
 package Entity;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,38 +20,47 @@ public class User {
         checkPassword(password);
     }
 
+    //Public method to add user to userList.
     public void addUserToList(User user) {
         this.userList.add(user);
     }
 
+    //Public method to get the name.
     public String getName() {
         return name;
     }
 
+    //Public method to get username.
     public String getUserName() {
         return userName;
     }
 
+    //Public method to get password.
     public String getPassword() {
         return password;
     }
 
+    //Public method to get the userList.
     public List<User> getUserList() {
         return userList;
     }
 
+    //Public method to change name.
     public void changeName(String name) {
         this.name = name;
     }
 
+    //Public method to change username.
     public void changeUserName(String userName) {
         this.userName = userName;
     }
 
+    //Public method change password.
     public void changePassword(String password) {
         this.password = password;
     }
 
+    //Public method to delete from the user list.
     public void deleteFromList(List<User> userList) {
         this.userList = userList;
     }
