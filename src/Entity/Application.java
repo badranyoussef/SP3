44e0c6cf3e2b4efb5c7efb75ce35016ad447bcb7
lassekh <lasse.kh.92@gmail.com>
2023-04-24 10:39:53 +0200

    Application Class
    Added methods to User and Media

---
+++ src/Entity/Application.java
@@ -0,0 +1,43 @@
+package Entity;
+
+import Utility.IO;
+import Utility.UI;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Application {
+    private List<User> users;
+    private Set<Media> medias;
+    private IO io;
+    private UI ui;
+    public void launchApplication(){
+
+    }
+    public void startMenu(){
+
+    }
+    public void createUser(){
+
+    }
+    public void login(){
+
+    }
+    public void mainMenu(){
+
+    }
+    public Media search(String input){
+        //todo: add return statement
+    }
+    public Set<Media> filter(int i){
+        //todo: add return statement
+    }
+    public void chooseMedia(){
+
+    }
+    public void playMedia(){
+
+    }
+}
