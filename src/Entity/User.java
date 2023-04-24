3965ba224446f47c0f2c03ea8ee3a6e79ad66026
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-04-24 08:49:33 +0200

    user class done

---
+++ src/Entity/User.java
@@ -0,0 +1,33 @@
+package Entity;
+
+public class User {
+
+    //Three private attributes for the User.
+    private String name;
+    private String userName;
+    private String password;
+
+
+    //Public constructor to create a user.
+    public User(String name, String userName, String password) {
+        this.name = name;
+        checkUserName(userName);
+        checkPassword(password);
+    }
+
+    //Private method to check if username can be used.
+    private String checkUserName(String userName) {
+        if (userName.length() > 6) {
+            return this.userName = userName;
+        }
+        return checkUserName(userName);
+    }
+
+    //Private method to check if password can be used.
+    private String checkPassword(String password) {
+        if (password.length() > 8) {
+            return this.password = password;
+        }
+        return checkPassword(password);
+    }
+}
