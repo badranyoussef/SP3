39b2640508bd93015a0c9447d5248f2fd1843f16
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-04-24 10:39:25 +0200

    Updated

---
+++ src/Entity/Serie.java
@@ -0,0 +1,10 @@
+package Entity;
+
+public class Serie extends Media {
+
+    Serie(String title, String category, float rating, int releaseYear) {
+        super(title, category, rating, releaseYear);
+    }
+
+
+}
